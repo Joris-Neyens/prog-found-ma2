99c1b211edc3a98d301e7b7c08f943f987cd105e
Joris Neyens <neyens01@gmail.com>
2020-01-12 12:31:20 +0100

    corrections after last ma feedback

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 // question 1
 
-// var petArray = ["cat", "cow", "dog"];
+var petArray = ["cat", "cow", "dog"];
 
 // question 2
 
-// console.log (petArray[1]);
+console.log(petArray[1]);
 
 // question 3
 
-// petArray.push("sheep");
+petArray.push("sheep");
 
 //question 4
 
-// console.log (petArray.length);
+console.log(petArray.length);
 
 // question 5
 
-// var catObject = {
-    name: "Max" ,
-    colour: "Black" ,
-    age: 3 ,
+var catObject = {
+    name: "Max",
+    colour: "Black",
+    age: 3
 };
 
 // question 6
 
-// console.log (catObject.age);
+console.log(catObject.age);
 
 // question 7
 
-// var catArray = [
+var catArray = [
     {
-        name: "Max" ,
-        colour: "Black" ,
-        age: 3 ,
+        name: "Max",
+        colour: "Black",
+        age: 3,
     }
 ];
 
 // question 8
 
-// for(var i=0; i < catArray.length; i++) {
+for(var i = 0; i < catArray.length; i++) {
     console.log(catArray[i]);
 };
 
 // question 9
 
-// function logToConsole (animal) {
+function logToConsole(animal) {
     console.log(animal);
 };
 
 // question 10
 
-// logToConsole("Donkey");
+logToConsole("Donkey");
